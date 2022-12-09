72cf9307023ec5b963b96527abf8237f6aa3bbba
6 - uy ishi <jaloliddinabdusattorov83@gmail.com>
2022-12-09 16:30:50 +0500

    end

---
+++ java.js
@@ -0,0 +1,6 @@
+let r = Math.floor(Math.random() * 10) + 1; //random number
+
+let input = prompt("1dan 10gacha bir sonni o'ylang")
+
+console.log("Men", r, "sonini o'ylagandim.", "six esa", input,"ni o'yladingiz");
+console.log(`Men ${r} sonini o'ylagan edim, siz ${input} sonini yozdingiz`);
