0d2b09049d49522954f107640c20f078391ac25e
6 - uy ishi <jaloliddinabdusattorov83@gmail.com>
2022-12-09 16:36:31 +0500

    the

--- java.js
+++ java.js
@@ -2,5 +2,13 @@ let r = Math.floor(Math.random() * 10) + 1; //random number
 
 let input = prompt("1dan 10gacha bir sonni o'ylang")
 
-console.log("Men", r, "sonini o'ylagandim.", "six esa", input,"ni o'yladingiz");
+// console.log("Men", r, "sonini o'ylagandim.", "six esa", input,"ni o'yladingiz");
 console.log(`Men ${r} sonini o'ylagan edim, siz ${input} sonini yozdingiz`);
+
+if(input = r){
+    alert("Topdingiz");
+}else if(input > r){
+    alert("Katta son o'yladingiz")
+}else if(input < r){
+    alert("Kichik son o'yladingiz")
+}
